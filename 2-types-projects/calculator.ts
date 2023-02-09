01e723a33ea11cfe85011f7c3a8cbc8e574e6a55
yshush <tjdgns7533@gmail.com>
2023-02-10 01:28:14 +0900

    position - game

--- 2-types-projects/calculator.ts
+++ 2-types-projects/calculator.ts
@@ -21,6 +21,6 @@ function calculate(command: Command, a: number, b: number): number {
     case 'remainder':
       return a % b;
     default:
-      throw Error('unknown command');
+      throw new Error('unknown command');
   }
 }
